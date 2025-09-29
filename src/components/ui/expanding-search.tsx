319f8dcf9930cf2f0eaee3487efb9b22b51074ef
Chenuka Medawela <chenuka.medawela@aiesec.sg>
2025-09-29 08:44:54 +0000

    make is smaller from 25%

--- src/components/ui/expanding-search.tsx
+++ src/components/ui/expanding-search.tsx
@@ -93,7 +93,7 @@ export const ExpandingSearch = React.forwardRef<HTMLDivElement, ExpandingSearchP
                         className="absolute right-2 top-1/2 -translate-y-1/2 h-5 w-5 rounded-full"
                         onClick={handleClear}
                     >
-                        <X className="h-2 w-2" />
+                        <X style={{ width: '6px', height: '6px' }} />
                         <span className="sr-only">Clear search</span>
                     </Button>
                 )}
